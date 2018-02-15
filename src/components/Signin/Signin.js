5e5d6128af719101f7d342677d44bb0c7dbf1dbf
Max <maxmal86@gmail.com>
2018-02-14 21:01:09 -0600

    Signin email to lower case validation

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -23,7 +23,7 @@ class Signin extends React.Component {
 			method: "post",
 			headers: {"Content-Type": "application/json"},
 			body: JSON.stringify({
-				email: this.state.signInEmail,
+				email: this.state.signInEmail.toLowerCase(),
 				password: this.state.signInPassword
 			})
 		})
